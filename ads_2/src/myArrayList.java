3660a5b29305c322988eca047d95b65dd1b88fcd
yernazarius <72621687+yernazarius@users.noreply.github.com>
2023-04-16 23:51:05 +0600

    Update myArrayList.java

--- ads_2/src/myArrayList.java
+++ ads_2/src/myArrayList.java
@@ -1,69 +1,125 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 
-public class myArrayList<T> implements myList<T> {
-    private int size = 0;
+public class myArrayList<T> implements myList<T>{
     private Object[] data;
+    private int size;
 
     public myArrayList() {
-        this(10);
+        this.data = new Object[10];
+        this.size = 0;
     }
-    public myArrayList(int size) {
-        data = new Object[size];
+    @Override
+    public int size(){
+        return size;
     }
+    @Override
+    public boolean contains(Object o) {
+        for(int i = 0; i < size; i++){
+            if(data[i].equals(o)) return true;
+        }
 
-    private void increaseSize() {
-        Object[] temp = new Object[data.length * 2];
-        System.arraycopy(data, 0, temp, 0, data.length);
+        return false;
+    }
+    private void increaseCapacity(){
+        Object[] temp = new Object[size * 2];
+        for(int i = 0; i < size; i++) {
+            temp[i] = data[i];
+        }
         data = temp;
     }
 
     @Override
-    public void add(T item) {
-        if (size == data.length) {
-            increaseSize();
-        }
+    public void add(T item){
+        if (size >= data.length) increaseCapacity();
         data[size++] = item;
     }
-    private void sizeCheck(int index) {
-        if (index > data.length) throw new IndexOutOfBoundsException();
-    }
-    @Override
-    public void remove(int index) {
-        sizeCheck(index);
-        for (int i = index; i < size; i++) {
-            data[i] = data[i + 1];
-        }
-        size--;
-    }
 
     @Override
-    public T get(int index) {
-        sizeCheck(index);
-        return (T) data[index];
+    public void add(T item, int index) {
+        if (index > size) throw new IndexOutOfBoundsException();
+        if (size() == data.length) increaseCapacity();
+        for (int i = size; i > index; i--)
+            data[i] = data[i - 1];
+
+        data[index] = item;
+        size++;
     }
 
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
     public Iterator<T> iterator() {
         return new MyIterator();
     }
 
-    private class MyIterator implements Iterator<T> {
-        private int cursor;
-
+    private class MyIterator implements Iterator<T>{
+        private int index;
         @Override
         public boolean hasNext() {
-            return cursor < size();
+            return index < size;
         }
 
         @Override
         public T next() {
-            return get(cursor++);
+            return get(index++);
         }
+    }
+    private void private_remove(int index) {
+        if (index >= size) throw new IndexOutOfBoundsException();
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[size - 1] = null;
+        size--;
+    }
+    @Override
+    public boolean remove(T item) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(item)) {
+                private_remove(i);
+                return true;
+            }
+        }
+        size--;
+        return false;
+    }
+    @Override
+    public T remove(int index) {
+        T temp = get(index);
+        for (int i = index; i < size - 1; i++){
+            data[i] = data[i + 1];
+        }
+        data[size--] = null;
+        return temp;
+    }
+    @Override
+    public T get(int index) {
+        if (index >= size) throw new IndexOutOfBoundsException();
+        return (T) data[index];
+    }
+    @Override
+    public void clear() {
+        size = 0;
+        data = new Object[10];
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for(int i = 0; i < size; i++){
+            if(data[i].equals(o)) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for(int i = size - 1; i >= 0; i--){
+            if(data[i].equals(o)) return i;
+        }
+        return -1;
+    }
+    @Override
+    public void sort() {
+        Arrays.sort(data, 0, size);
     }
 
 }
