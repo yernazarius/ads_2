baf0d752916ac1f6eee54626bf3513b2b765c2e8
yernazarius <72621687+yernazarius@users.noreply.github.com>
2023-04-16 23:51:24 +0600

    Update Main.java

--- ads_2/src/Main.java
+++ ads_2/src/Main.java
@@ -1,19 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        myArrayList<Integer> list = new myArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(50);
-        list.add(19);
-        for (var wow: list) {
-            System.out.print(wow + " ");
-        }
+        myArrayList<Integer> arrayList = new myArrayList<>();
+        arrayList.add(10);
+        arrayList.add(12);
+        arrayList.add(423);
+        arrayList.add(11);
+        arrayList.add(4);
+        arrayList.add(511);
+        for (int a: arrayList)
+            System.out.print(a + " ");
+
+        arrayList.remove(2);
         System.out.println();
-        System.out.println(list.get(3));
-        list.remove(3);
-        for (var wow: list) {
-            System.out.print(wow + " ");
-        }
-//        System.out.println(list);
+        for (int a: arrayList)
+            System.out.print(a + " ");
+        System.out.println();
+
+        arrayList.remove((Integer)12);
+        for (int a: arrayList) System.out.print(a + " ");
+        System.out.println();
+
+        System.out.println("Size" + arrayList.size());
+        System.out.println("Getting 3 element " + arrayList.get(2));
+        System.out.println("any 423 in array? " + arrayList.contains(5));
+        System.out.println("any 9999 in array? " + arrayList.contains(9));
+        arrayList.add(1111,4);
+        System.out.println("after inserting");
+        for (int a: arrayList) System.out.print(a + " ");
+        arrayList.sort();
+        System.out.println("Sorted ArrayList: ");
+        for (int a: arrayList) System.out.print(a + " ");
+
     }
 }
